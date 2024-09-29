4c290912669670f30a21cfffd6ec0eb85c986c97
Toshikazu Matsumura <matutosi@gmail.com>
2024-09-30 03:19:26 +0900

    pivotea

--- R/global.R
+++ R/global.R
@@ -1,7 +1,9 @@
 if(!require("colourpicker"))   install.packages("colourpicker")
+if(!require("pivotea"))        install.packages("pivotea")
 if(!require("openxlsx"))       install.packages("openxlsx")
 if(!require("shiny"))          install.packages("shiny")
 
 source("convex.R")
 source("file.R")
 source("highlight.R")
+source("pivotea.R")
